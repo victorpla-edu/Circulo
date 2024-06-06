a7a63f46c015879e679fcbf2d5b9193e6bcdf7a9
FilipLand <filip9038@gmail.com>
2024-06-06 08:47:18 +0200

    Comento lo que hace el metodo calcular el Area, la excepcion y lo que retorna

--- Circulo.java
+++ Circulo.java
@@ -14,12 +14,18 @@ public class Circulo {
 
     /* Esta clase está descontinuada, se debe usar en su lugar calcularArea() */
     /**
-     * @deprecated calcularArea se debe usar la variable calcularArea
+     * @deprecated calcularArea se debe usar la nueva variable calcularArea
      */
     public double calculoArea() {
         return Math.PI * Math.pow(radio, 2);
     }
 
+    /**
+     * @param calcularArea metodo que te calcula el area
+     * @throws IllegalArgumentException Si el radio es negativo te suelta una
+     *                                  exsepción
+     * @return devuelve el resultado de la operacion de PI * radio al cuadrado
+     */
     public double calcularArea() {
         if (radio < 0) {
             throw new IllegalArgumentException("El radio no puede ser negativo");
